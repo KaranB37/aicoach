a248d0b22dd6fe2ba5a9704a40d53d7151588c95
KaranB37 <86348214+KaranB37@users.noreply.github.com>
2024-09-24 19:34:42 +0530

    added scroll section

--- src/Components/Scroll/Scroll.js
+++ src/Components/Scroll/Scroll.js
@@ -13,11 +13,27 @@ const Scroll = () => {
         </p>
       </div>
       <div className={styles.container}>
-        {/* <div className={styles.left}>
-          <div className={styles.scrollingText}>
-            <p>Your scrolling text goes here...</p>
+        <div className={styles.left}>
+          <div className={styles.textcon}>
+            <span className={styles.text1}>
+              Get instant access to{" "}
+              <span className={styles.sp}>AgentCoach.ai</span> and start
+              experiencing expert advice tailored just for you in{" "}
+              <span className={styles.sp}>
+                Real Estate, Sales, Negotiation, Marketing, and Motivation.
+              </span>
+            </span>
+            <span className={styles.text2}>
+              Best of all, you can get started right now, completely free! Take
+              advantage of this opportunity to boost your career and see the
+              difference AI-powered coaching can make.
+            </span>
+            <span className={styles.text3}>
+              Sign up for free & unlock your potential today!
+            </span>
           </div>
-        </div> */}
+          {/* <p>Your scrolling text goes here...</p> */}
+        </div>
         <div className={styles.right}>
           <video
             src={video}
